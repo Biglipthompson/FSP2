30ec9318341c818466a143fda48a7c88d893cf40
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2021-01-22 22:52:36 -0500

    Commit
    Commit

--- DPAF/js/script.js
+++ DPAF/js/script.js
@@ -12,28 +12,28 @@ This function will create and insert/append the elements needed to display a "pa
 // A list parameter to represent student data that will be passed as an argument when the function is called.
 //A page parameter to represent the page number that will be passed as an argument when the function is called.
 
-function showPage(list, page){
-var startIndex = (page * 9 ) - 9;
-var endIndex = page * 9;
-const studentList = document.querySelector('.student-list');
-   studentList.innerHTML = ' ';
-   for( i = 0; list.length; i++){
-   if( i >= startIndex && i < endIndex){
-     var studentItem = 
-      `<li class="student-item cf">
-      <div class="student-details">
-      <img class="avatar" src="${studentList[i].picture.thumbnail}" alt="Profile Picture">
-      <h3>${studentList[i].name.first} '' ${studentList[i].name.last}</h3>
-      <span class="email">${studentList[i].email}</span>
-      </div>
-      <div class="joined-details">
-      <span class="date">${studentList[i].registered.date}</span>
-      </div>
-      </li>`
-      studentList.innerHTML = studentItem;
-      }
-   }
-} 
+// function showPage(list, page){
+// var startIndex = (page * 9 ) - 9;
+// var endIndex = page * 9;
+// let studentList = document.querySelector('.student-list');
+//    studentList.innerHTML = ' ';
+//    for( let i = 0; list.length; i++){
+//    if( i >= startIndex && i < endIndex){
+//      var studentItem = 
+//       `<li class="student-item cf">
+//       <div class="student-details">
+//       <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+//       <h3>${list[i].name.first} ${list[i].name.last}</h3>
+//       <span class="email">${list[i].email}</span>
+//       </div>
+//       <div class="joined-details">
+//       <span class="date">${list[i].registered.date}</span>
+//       </div>
+//       </li>`
+//       studentList.innerHTML = studentItem;
+//       }
+//    }
+// } 
 
 /*
 Create the addPagination function
@@ -47,3 +47,6 @@ function addPagination (){
 
 
 // Call functions
+
+
+console.log(showPage)
