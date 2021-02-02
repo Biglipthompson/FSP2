1a7d677d899ed6139085e0c6610fcc0b2b450590
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2021-02-02 10:36:33 -0500

    Commit
    Added some code comments.

--- DPAF/js/script.js
+++ DPAF/js/script.js
@@ -1,27 +1,32 @@
+// these are my variables to grab the list and the ul
 const linkList = document.querySelector('.link-list');
 const studentList = document.querySelector('.student-list');
+
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
-
 // A list parameter to represent student data that will be passed as an argument when the function is called.
 //A page parameter to represent the page number that will be passed as an argument when the function is called.
-
 function showPage(list, page){
 const startIndex = ((page * 9 ) - 9);
 const endIndex = (page * 9);
 
 
+
+// this gets the student list class
 let studentList = document.querySelector('.student-list');
 studentList.innerHTML = " ";
 
+//for loop that iterates over the list
    for(let i = 0; i < list.length; i++){
 
    if( i >= startIndex && i < endIndex){
       
+
+   // this gets the html and replaces the code with this new code
      const studentItem = 
       `<li class="student-item cf">
       <div class="student-details">
@@ -34,6 +39,7 @@ studentList.innerHTML = " ";
       </div>
       </li>`;
       studentList.insertAdjacentHTML("beforeend", studentItem);
+
       // console.log(list);
       // console.log(page);
 
@@ -45,15 +51,17 @@ studentList.innerHTML = " ";
 // Create the addPagination function
 // This function will create and insert/append the elements needed for the pagination buttons
 // */
-
 function addPagination(list) {
 
    const numOfPages = Math.ceil(list.length / 9);
 
+   //this grabs the link list class
    const linkList = document.querySelector('.link-list');
 
+   //sets the inner html to the linklist to empty to make the html blank
    linkList.innerHTML = " ";
 
+   //This loops is going to go over the buttons for the button classes
    for (let i = 1; i <= numOfPages; i++){
    const button = `
    <li>
@@ -65,6 +73,7 @@ function addPagination(list) {
    pageButton.className = 'active';   
  
    }
+// This event listener gets the clicked button ans changes its class to active 
 linkList.addEventListener('click', (e) => {
    const clickEvent = e.target;
       if (clickEvent.tagName === 'BUTTON'){
